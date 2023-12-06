299c432f5f94a9bb8b257a9ffbf84e396c7df133
Daria Kolodzey <kolodaria@gmail.com>
2023-12-06 16:46:54 +0100

    dev: Finish day 5 part 2

--- 5b.swift
+++ 5b.swift
@@ -1,14 +1,14 @@
-// Work in progress for day 5 part 2
+// Solves day 5 part 2
 // Run from the command line as follows:
 // $ swift 5b.swift 5.example
 // ...[some debug data]...
-// answer: ???
+// answer: 46
 
 import Foundation
 import RegexBuilder
 
 struct ValueRange {
-    let start: Int
+    var start: Int
     let length: Int
 }
 
@@ -93,55 +93,72 @@ let dataRegex = Regex {
     }, transform: parseMaps)
 }
 
-func performMap(sources: inout [ValueRange], with mapping: AlmanacRange) -> [ValueRange]
+func aIsStrictlyInsideB(a: ValueRange, b: ValueRange) -> Bool
 {
-    var newValues: [ValueRange] = []
-    var unaffectedValues: [ValueRange] = []
-    for source in sources {
-        // no mapping
-        if (source.start + source.length < mapping.sourceStart 
-        || source.start >= mapping.sourceStart + mapping.length) {
-            unaffectedValues.append(source)
-            continue
-        }
-        let diff = mapping.destinationStart - mapping.sourceStart
-        // whole range mapped
-        if (source.start >= mapping.sourceStart 
-        && source.start + source.length <= mapping.sourceStart + mapping.length) {
-            newValues.append(ValueRange(start: source.start + diff, length: source.length))
-            continue
-        }
-        // mapped values in the middle
-        if (source.start < mapping.sourceStart
-        && source.start + source.length > mapping.sourceStart + mapping.length) {
-            let leftRemaining = ValueRange(start: source.start, length: mapping.sourceStart - source.start)
-            let rightRemaining = ValueRange(start: mapping.sourceStart + mapping.length, 
-                length: source.start + source.length - mapping.sourceStart - mapping.length)
-            newValues.append(ValueRange(start: mapping.destinationStart, length: mapping.length))
-            unaffectedValues.append(contentsOf: [leftRemaining, rightRemaining])
-            continue
-        }
-        // mapped values in the beginning
-        if (source.start >= mapping.sourceStart 
-        && source.start + source.length > mapping.sourceStart + mapping.length) {
-            let rightRemaining = ValueRange(start: mapping.sourceStart + mapping.length, 
-                length: source.start + source.length - mapping.sourceStart - mapping.length)
-            newValues.append(ValueRange(start: mapping.destinationStart, length: mapping.length))
-            unaffectedValues.append(rightRemaining)
-            continue
-        }
-        // mapped values in the end
-        if (source.start < mapping.sourceStart
-        && source.start + source.length <= mapping.sourceStart + mapping.length) {
-            let leftRemaining = ValueRange(start: source.start, length: mapping.sourceStart - source.start)
-            newValues.append(ValueRange(start: mapping.destinationStart, length: source.start + source.length - mapping.sourceStart))
-            unaffectedValues.append(leftRemaining)
-            continue
-        }
-        print("something went wrong with source \(source) and mapping \(mapping)")
+    return a.start >= b.start && a.start + a.length <= b.start + b.length
+}
+
+func aIsStrictlyOutsideB(a: ValueRange, b: ValueRange) -> Bool
+{
+    return ((a.length + a.start - 1 < b.start)
+        || (a.start > b.start + b.length - 1))
+}
+
+enum RangeEventType {
+    case rangeStart
+    case rangeEnd // this point is excluded
+    case knife
+}
+
+struct RangeEvent {
+    let x: Int
+    let type: RangeEventType
+}
+
+func cut(_ range: ValueRange, with knife: ValueRange) -> [ValueRange]
+{
+    if (aIsStrictlyInsideB(a: range, b: knife) 
+    || aIsStrictlyOutsideB(a: range, b: knife)) {
+        return [range]
     }
-    sources = unaffectedValues
-    return newValues
+    var ans:[ValueRange] = []
+    var events = [
+        RangeEvent(x: range.start, type: .rangeStart),
+        RangeEvent(x: range.start + range.length, type: .rangeEnd),
+        RangeEvent(x: knife.start, type: .knife),
+        RangeEvent(x: knife.start + knife.length, type: .knife)
+    ]
+    events.sort(by: {$0.x < $1.x })
+    var latestStart = -1
+    var balance = 0
+    for event in events {
+        if event.type == .rangeStart {
+            balance += 1
+            latestStart = event.x
+        } else if event.type == .rangeEnd {
+            let length = event.x - latestStart
+            if (length > 0) {
+                ans.append(ValueRange(start: latestStart, length: event.x - latestStart))
+            }
+            balance -= 1
+        } else if event.type == .knife && balance > 0 {
+            let length = event.x - latestStart
+            if (length > 0)
+            {
+                ans.append(ValueRange(start: latestStart, length: event.x - latestStart))
+                latestStart = event.x
+            }
+        }
+    }
+    return ans
+}
+
+func chop(_ ranges: [ValueRange], with knife: ValueRange) -> [ValueRange] {
+    var ans: [ValueRange] = []
+    for range in ranges {
+        ans.append(contentsOf: cut(range, with: knife))
+    }
+    return ans
 }
 
 let dataFile = CommandLine.arguments[1]
@@ -150,15 +167,18 @@ let parsedData = rawData.firstMatch(of: dataRegex)!.output
 var values = parsedData.1
 var maps = parsedData.2
 for map in maps {
-    var newValues: [ValueRange] = []
-    for value in values {
-        var sourceValues = [value]
-        for mapping in map.ranges {
-            newValues.append(contentsOf: performMap(sources: &sourceValues, with: mapping))
-        }
-        newValues.append(contentsOf: sourceValues)
+    for mappingRange in map.ranges {
+        values = chop(values, with: ValueRange(start: mappingRange.sourceStart, length:mappingRange.length))
     }
-    values = newValues
     print(values)
+    for i in 0..<values.count {
+        for mappingRange in map.ranges {
+            if aIsStrictlyInsideB(a: values[i], b: ValueRange(start: mappingRange.sourceStart, length: mappingRange.length)) {
+                values[i].start = values[i].start - mappingRange.sourceStart + mappingRange.destinationStart
+                break
+            }
+        }    
+    }
+    
 }
-print(values.min(by: {$0.start < $1.start})!.start)
+print("answer:", values.min(by: {$0.start < $1.start})!.start)
